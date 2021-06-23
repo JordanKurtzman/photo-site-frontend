f19c196c06fd532ac124ab9d802d37bc0c4d0971
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-06-22 18:18:50 -0700

    contact form js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {displayPetPhoto, displayMaternityPhoto, displayPortraitPhoto} from './portfolio'
 import Glide, { Autoplay } from "@glidejs/glide"
-import { normalizeResponse} from './contact'
 
 const toggleNav = document.querySelector('#toggleNav')
 const primaryMenu = document.querySelector('#primarymenu')
@@ -8,6 +7,7 @@ const dropdownBtn = document.querySelector('#dropdownbtn')
 const submenu = document.querySelector('#submenu')
 const submitBtn = document.querySelector('#btnSubmit')
 const formElements = document.querySelectorAll("form");
+
 
 toggleNav.addEventListener('click', () => {
     primaryMenu.classList.toggle('active')
@@ -29,38 +29,89 @@ var glide = new Glide('#hero', {
     perView: 1
 });
 
-// const formSubmissionHandler = (event) => {
-//     event.preventDefault();
 
-//     const formElement = event.target,
-//         { action, method } = formElement,
-//         body = new FormData(formElement);
-
-//     fetch(action, {
-//         method,
-//         body
-//     })
-//         .then((response) => response.json())
-//         // .then((response) => normalizeResponse(action, response))
-//         .then((response) => {
-//             alert(response.message);
-
-//             if (response.isSuccess) {
-//                 formElement.reset();
-//             }
-//         })
-//         .catch((error) => {
-//             alert("Check the console for the error details.");
-//             console.log(error);
-//         });
-// };
-
-// formElements.forEach((formElement) =>
-//     formElement.addEventListener("submit", formSubmissionHandler)
-// );
 
 
 glide.mount()
 
 
+const stripHtml = (string) => string.replace(/(<([^>]+)>)/gi, "");
 
+const initialState = {
+    isSuccess: false,
+    message: "",
+    validationError: {}
+};
+
+const normalizeResponse = (url, response) => {
+    if (
+        url.match(/wp-json\/contact-form-7\/v1\/contact-forms\/\d+\/feedback/)
+    ) {
+        return normalizeContactForm7Response(response);
+    }
+    return {
+        ...initialState,
+        ...{
+            message: "Are you submitting to the right URL?"
+        }
+    };
+};
+
+const normalizeContactForm7Response = (response) => {
+    const isSuccess = response.status === "mail_sent";
+    const message = response.message;
+    const validationError = isSuccess
+        ? {}
+        : Object.fromEntries(
+            response.invalid_fields.map((error) => {
+                const key = /cf7[-a-z]*.(.*)/.exec(error.into)[1];
+
+                return [key, error.message];
+            })
+        );
+
+    return {
+        isSuccess,
+        message,
+        validationError
+    };
+};
+
+const wpForm = () => {
+    return {
+        ...initialState,
+        submit() {
+            const formElement = this.$refs.form,
+                { action, method } = formElement,
+                body = new FormData(formElement);
+
+            fetch(action, {
+                method,
+                body
+            })
+                .then((response) => response.json())
+                .then((response) => normalizeResponse(action, response))
+                .then((response) => {
+                    this.updateState(response);
+
+                    if (this.isSuccess) {
+                        formElement.reset();
+                    }
+                })
+                .catch((error) => {
+                    this.updateState({
+                        ...initialState,
+                        ...{
+                            message: "Check the console for the error details."
+                        }
+                    });
+                    console.log(error);
+                });
+        },
+        updateState(newState) {
+            Object.keys(newState).forEach((key) => (this[key] = newState[key]));
+        }
+    };
+};
+
+window.wpForm = wpForm;
