419ea3c5fb9bc7776fd4bb8b72992e272bf562c2
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-06-29 21:33:00 -0700

    trying jquery ajax call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {displayPetPhoto, displayMaternityPhoto, displayPortraitPhoto} from './portfolio'
 import Glide, { Autoplay } from "@glidejs/glide"
+import {$, jQuery} from 'jquery'
+
 
 const toggleNav = document.querySelector('#toggleNav')
 const primaryMenu = document.querySelector('#primarymenu')
@@ -7,6 +9,9 @@ const dropdownBtn = document.querySelector('#dropdownbtn')
 const submenu = document.querySelector('#submenu')
 const formElem = document.getElementById('formElem')
 const messageArea = document.getElementById('messageArea')
+
+window.$ = $;
+window.jQuery = jQuery;
 
 toggleNav.addEventListener('click', () => {
     primaryMenu.classList.toggle('active')
@@ -32,63 +37,47 @@ var glide = new Glide('#hero', {
 
 glide.mount()
 
-// formElem.addEventListener('submit', (e) => {
-    
-//     let request = new XMLHttpRequest()
-//     request.open("POST", 'https://api.chloemedranophotography.com/wp-json/contact-form-7/v1/contact-forms/54/feedback')
+
+
+
+// const submitFormData = () => {
+//     let xmlhttp = new XMLHttpRequest();
+
+//     xmlhttp.onreadystatechange = function () {
+//         if (xmlhttp.readyState == 4) {   // XMLHttpRequest.DONE == 4
+//             if (xmlhttp.status == 400) {
+//                 alert('There was an error 400');
+//             }
+//             else {
+//                 alert('something else other than 200 was returned');
+//             }
+//         }
+//     };
+//     e.preventDefault()
+//     xmlhttp.open("POST", "https://api.chloemedranophotography.com/wp-json/contact-form-7/v1/contact-forms/54/feedback", true);
 //     let formData = new FormData(formElem)
-//     request.send(formData)
+//     xmlhttp.send(formData)
+// }
+
+
+// formElem.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     submitFormData()
 //     formElem.reset()
 // })
 
 
 
-const submitFormData = () => {
-    let xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4) {   // XMLHttpRequest.DONE == 4
-            if (xmlhttp.status == 400) {
-                alert('There was an error 400');
-            }
-            else {
-                alert('something else other than 200 was returned');
-            }
-        }
-    };
-    e.preventDefault()
-    xmlhttp.open("POST", "https://api.chloemedranophotography.com/wp-json/contact-form-7/v1/contact-forms/54/feedback", true);
-    let formData = new FormData(formElem)
-    xmlhttp.send(formData)
-}
-
-
-formElem.addEventListener('submit', (e) => {
-    e.preventDefault()
-    submitFormData()
-    formElem.reset()
+$(document).ready(function(){
+    $('#formElem').on('submit', function (event) {
+        event.preventDefault();
+        let request = $.ajax({
+            url: "https://api.chloemedranophotography.com/wp-json/contact-form-7/v1/contact-forms/54/feedback",
+            type: "post",
+            data: new FormData(this)
+        });
+        $('#formElem').trigger('reset');
+    })
 })
-
-
-
-
-// const submitFormData = async () => {
-//     try {
-//         let response = await fetch('https://api.chloemedranophotography.com/wp-json/contact-form-7/v1/contact-forms/54/feedback', {
-//             method: 'POST',
-//             body: new FormData(formElem)
-//         });
-//         let result = await response.json()
-//         alert(result.message)
-//         // const successMessage = document.createElement('p')
-//         // successMessage.textContent = 'Thanks for getting in touch! We will get in touch with you shortly.'
-//         // messageArea.appendChild(successMessage)
-//     }
-//     catch (error) {
-//         console.log(error)
-//     }
-    
-// }
-
-
 
